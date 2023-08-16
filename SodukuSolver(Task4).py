6f05d8d53216fcb0dcd8c3981d3c62c36ed23b47
Mangesh Pangam <135334519+Mangesh2704@users.noreply.github.com>
2023-08-16 18:02:47 +0530

    Add files via upload

---
+++ SodukuSolver(Task4).py
@@ -0,0 +1,46 @@
+def is_valid(board, row, col, num):
+    # Check if 'num' is not in current row, column, and 3x3 subgrid
+    for i in range(9):
+        if board[row][i] == num or board[i][col] == num:
+            return False
+    subgrid_row, subgrid_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(subgrid_row, subgrid_row + 3):
+        for j in range(subgrid_col, subgrid_col + 3):
+            if board[i][j] == num:
+                return False
+    return True
+
+def solve_sudoku(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+                        if solve_sudoku(board):
+                            return True
+                        board[row][col] = 0  # If not valid, backtrack
+                return False  # No valid number found, need to backtrack
+    return True  # All cells filled, puzzle solved
+
+def print_sudoku(board):
+    for row in board:
+        print(" ".join(map(str, row)))
+
+def main():
+    print("Welcome to Sudoku Solver!")
+    print("Please enter the Sudoku puzzle, row by row, using 0 for empty cells.")
+
+    sudoku_board = []
+    for _ in range(9):
+        row = list(map(int, input().split()))
+        sudoku_board.append(row)
+
+    if solve_sudoku(sudoku_board):
+        print("\nSudoku Solved:")
+        print_sudoku(sudoku_board)
+    else:
+        print("\nNo solution exists for the given Sudoku puzzle.")
+
+if __name__ == "__main__":
+    main()
